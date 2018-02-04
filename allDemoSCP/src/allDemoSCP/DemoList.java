d924aa0b9f464ee78bb1bd4cc41f5a75f10b7af6
jyotinighot <imaheshnighot@gmail.com>
2018-02-05 01:55:15 +0530

    Samplecode

---
+++ allDemoSCP/src/allDemoSCP/DemoList.java
@@ -0,0 +1,11 @@
+package allDemoSCP;
+import java.util.*;
+public class DemoList {
+public static void main(String[] args) {
+ArrayList l1=new ArrayList();
+l1.add("ABC");
+l1.add(10);
+l1.add("XYZ");
+System.out.println(l1);
+}
+}
